c2d30f7b58dc90ff181599833b08f761e20d683c
yawkat <me@yawk.at>
2016-09-01 14:15:54 +0200

    Karma graph!

---
+++ src/main/resources/at/yawk/katbot/web/static/karma.js
@@ -0,0 +1,91 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+katbotApp.component('karma', {
+    templateUrl: "karma.template.html",
+    controller: function KarmaController($scope, $http, $sanitize) {
+        $scope.entries = [];
+        $scope.search = "";
+        $scope.page = 0;
+
+        function update() {
+            $http.get('api/karma?search=' + encodeURI($scope.search) + "&page=" + $scope.page).then(function (response) {
+                if (response.data.length == 0 && $scope.page > 0) {
+                    $scope.page = 0;
+                    update();
+                } else {
+                    $scope.entries = response.data;
+                }
+            });
+        }
+
+        update();
+        $scope.$watch("search", update);
+        $scope.$watch("page", update);
+
+        $scope.chartConfigBuilder = function (name) {
+            return {
+                title: {text: ''},
+                options: {
+                    chart: {zoomType: 'x'}
+                },
+                xAxis: {type: 'datetime'},
+                yAxis: {title: {text: ''}},
+                legend: {enabled: false},
+                loading: true,
+                func: function (plot) {
+                    console.log("fetch");
+                    $http.get("api/karma/" + name + "/history").then(function (response) {
+                        var data = [];
+                        var accumulated = 0;
+                        for (var i = 0; i < response.data.length; i++) {
+                            var entry = response.data[i];
+                            accumulated += entry.delta;
+                            data.push({
+                                x: Date.parse(entry.timestamp),
+                                y: accumulated,
+                                actor: entry.actor,
+                                delta: entry.delta
+                            })
+                        }
+                        plot.addSeries({
+                            type: 'area',
+                            step: 'left',
+                            showInLegend: false,
+                            animation: false,
+                            name: 'karma',
+                            data: data,
+                            color: '#55BF3B', // green
+                            negativeColor: '#DF5353', // red
+                            tooltip: {
+                                pointFormatter: function () {
+                                    var html = "";
+                                    html += "Karma: <b>" + this.y + "</b>";
+
+                                    html += "<br>Actor: ";
+                                    var actor = this.actor;
+                                    var hostIndex = actor.indexOf('@');
+                                    if (hostIndex != -1) {
+                                        var nick = $sanitize(actor.substring(0, hostIndex));
+                                        var host = $sanitize(actor.substring(hostIndex));
+                                        html += "<b>" + nick + "</b><span class='userHost'>" + host + "</span>";
+                                    } else {
+                                        html += "<b>" + $sanitize(actor) + "</b>";
+                                    }
+
+                                    html += "<br>Delta: <b>" + this.delta + "</b>";
+
+                                    return html;
+                                }
+                            }
+                        });
+                        plot.hideLoading();
+                    });
+                }
+            };
+        };
+    }
+});
---
+++ src/main/resources/at/yawk/katbot/web/static/security.js
@@ -0,0 +1,97 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+katbotApp.component('security', {
+    templateUrl: "security.template.html",
+    controller: function SecurityController($scope, $http) {
+        $scope.permissions = [];
+        $scope.roles = {};
+        $scope.users = [];
+
+        $scope.objectKeys = function (o) {
+            return Object.keys(o).filter(function (e) {
+                return o.hasOwnProperty(e)
+            });
+        };
+
+        $scope.deleteRole = function (role) {
+            $http.delete("api/security/roles/" + role).then(function () {
+                delete $scope.roles[role];
+            });
+        };
+
+        $scope.createRole = function (role) {
+            if (role) {
+                $http.put("api/security/roles/" + role, []).then(function () {
+                    $scope.roles[role] = [];
+                });
+            }
+        };
+
+        $scope.removePermission = function (role, permission) {
+            var newPermissions = angular.copy($scope.roles[role]);
+            newPermissions.splice(newPermissions.indexOf(permission), 1);
+            $http.put('api/security/roles/' + role, newPermissions).then(function () {
+                $scope.roles[role] = newPermissions;
+            });
+        };
+
+        $scope.addPermission = function (role, permission) {
+            var newPermissions = angular.copy($scope.roles[role]);
+            newPermissions.push(permission);
+            $http.put('api/security/roles/' + role, newPermissions).then(function () {
+                $scope.roles[role] = newPermissions;
+            });
+        };
+
+        function prepareUserForView(user) {
+            user = angular.copy(user);
+            user.$$roles = {};
+            for (var role in $scope.roles) {
+                if ($scope.roles.hasOwnProperty(role)) {
+                    user.$$roles[role] = function () {
+                        var finalRole = role;
+                        return function (newHasRole) {
+                            var oldIndex = user.roles.indexOf(finalRole);
+                            if (arguments.length) { // set
+                                var newRoles = angular.copy(user.roles);
+                                if (oldIndex != -1) {
+                                    newRoles.splice(oldIndex, 1);
+                                }
+                                if (newHasRole) {
+                                    newRoles.push(finalRole);
+                                }
+
+                                $http.put('api/security/users/' + user.username + '@' + user.host, newRoles).then(function () {
+                                    user.roles = newRoles;
+                                });
+                            } else { // get
+                                return oldIndex != -1;
+                            }
+                        };
+                    }();
+                }
+            }
+            return user;
+        }
+
+        $scope.addUser = function (nick, host) {
+            if (nick && host) {
+                $scope.users.push(prepareUserForView({username: nick, host: host, roles: []}));
+            }
+        };
+
+        $http.get('api/security/permissions').then(function (response) {
+            $scope.permissions = response.data;
+        });
+        $http.get('api/security/roles').then(function (response) {
+            $scope.roles = response.data;
+            $http.get('api/security/users').then(function (response) {
+                $scope.users = response.data.map(prepareUserForView);
+            });
+        });
+    }
+});
