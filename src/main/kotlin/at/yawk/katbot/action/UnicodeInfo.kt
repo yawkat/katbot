fce869aedd7e0b3cbb79598445563def4dd34b11
yawkat <me@yawk.at>
2017-07-10 15:29:52 +0200

    Don't use Character.getName, it sucks

--- src/main/kotlin/at/yawk/katbot/action/UnicodeInfo.kt
+++ src/main/kotlin/at/yawk/katbot/action/UnicodeInfo.kt
@@ -5,14 +5,27 @@ import at.yawk.katbot.EventBus
 import at.yawk.katbot.Subscribe
 import at.yawk.katbot.command.Command
 import at.yawk.katbot.sendMessageSafe
+import java.net.URL
 import javax.inject.Inject
 
 /**
  * @author yawkat
  */
+internal fun downloadInfo(): Map<Int, CharInfo> = URL("http://www.unicode.org/Public/UNIDATA/UnicodeData.txt")
+        .openStream().bufferedReader().lineSequence()
+        .map { it.split(';') }
+        .associate { Integer.parseInt(it[0], 16) to CharInfo(it[1], it[2]) }
+
+data class CharInfo(
+        val name: String,
+        val category: String
+)
+
 class UnicodeInfo @Inject constructor(
         val eventBus: EventBus
 ) {
+    private val info: Map<Int, CharInfo> by lazy { downloadInfo() }
+
     fun start() {
         eventBus.subscribe(this)
     }
@@ -29,7 +42,8 @@ class UnicodeInfo @Inject constructor(
             if (command.line.parameters.size < 3) usage()
 
             val op: (Int) -> String = when (command.line.parameters[1]) {
-                "name" -> Character::getName
+                "name" -> { it -> info[it]?.name ?: "None" }
+                "category" -> { it -> info[it]?.category ?: "None" }
                 "xid" -> Integer::toHexString
                 "did" -> Integer::toString
                 else -> usage()
