3b46a9465121a336d247b3425aa48d9d2ae65558
Jonas Konrad <me@yawk.at>
2020-01-31 21:14:03 +0100

    Another layout change

--- src/main/kotlin/at/yawk/katbot/action/NCov.kt
+++ src/main/kotlin/at/yawk/katbot/action/NCov.kt
@@ -96,7 +96,7 @@ class NCov @Inject constructor(private val eventBus: EventBus) {
                 val cells = row.select("td")
                 if (cells.size < 3) continue
                 val (name, cases, deaths) = cells
-                regions.add(Region(name.text().trim(), toInt(cases.text()), toInt(deaths.text())))
+                regions.add(Region(name.text().trim(), toInt(cases.ownText()), toInt(deaths.ownText())))
             }
             return regions
         }
